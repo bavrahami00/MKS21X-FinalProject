7341f62f13f6cba8b705bb03415643f7cbd41c9f
Derek Leung <dleung00@stuy.edu>
2019-01-04 10:15:38 -0500

    creates empty terminal when run

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -1,3 +1,35 @@
+import com.googlecode.lanterna.terminal.Terminal.SGR;
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.Key.Kind;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.Terminal.Color;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.LanternaException;
+import com.googlecode.lanterna.input.CharacterPattern;
+import com.googlecode.lanterna.input.InputDecoder;
+import com.googlecode.lanterna.input.InputProvider;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
+
 public class SpaceInvaders{
-  
+
+  public static void main(String[] args){
+
+    Terminal terminal = TerminalFacade.createTextTerminal();
+		terminal.enterPrivateMode();
+
+		TerminalSize size = terminal.getTerminalSize();
+		terminal.setCursorVisible(false);
+
+		boolean running = true;
+
+    while(running){
+
+      //lots of stuff to go here
+
+    }
+
+  }
+
 }
