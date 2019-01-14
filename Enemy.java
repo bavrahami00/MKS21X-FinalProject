b217114df4bd77b17962432e0548975cad6b1f83
bavrahami00 <bavrahami00@stuy.edu>
2019-01-13 22:49:31 -0500

    Made Enemy compile

--- Enemy.java
+++ Enemy.java
@@ -2,10 +2,15 @@ public class Enemy extends Being {
   public Enemy(int h, int a, int x, int y) {
     super(h,a,x,y);
   }
-  public boolean isOnEdge() {
+  public boolean isOnEdge(Enemy[] all) {
+    for (int x = 0; x < all.length; x++) {
+      if (getXPos() + 1 == all[x].getXPos() && getYPos() + 1 == all[x].getYPos()) {
+        return false;
+      }
+    }
     return true;
   }
   public boolean isDead() {
-    return (health == 0);
+    return (getHealth() == 0);
   }
 }
