d73e7157962568d66897f26263f9b88500d28f4a
bavrahami00 <bavrahami00@stuy.edu>
2019-01-14 10:13:57 -0500

    Removed unneeded move methods

--- User.java
+++ User.java
@@ -8,14 +8,8 @@ public class User extends Being {
     return (lives == 0 && getHealth() == 0);
   }
   public void move(int dir) {
-    if (dir == 0) {
-      setYPos(getYPos()-1);
-    }
     if (dir == 1) {
       setXPos(getXPos()+1);
-    }
-    if (dir == 2) {
-      setYPos(getYPos()+1);
     }
     if (dir == 3) {
       setXPos(getXPos()-1);
