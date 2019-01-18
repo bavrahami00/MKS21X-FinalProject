14779036fd1a8b572cd121ea0df3c9fae0c29570
bavrahami00 <bavrahami00@stuy.edu>
2019-01-18 10:06:55 -0500

    Created each Powerup

--- Powerup.java
+++ Powerup.java
@@ -14,9 +14,23 @@ public class Powerup implements HasLocation {
   public int getYPos() {
     return yPos;
   }
-  public void implement(User u) {
+  public int implement(User u,int score,barrier shields,int shootTime) {
     if (type.equals("Life")) {
       u.addLife();
     }
+    else if (type.equals("Score")) {
+      score += 150;
+    }
+    else if (type.equals("Barrier")) {
+      shields = new Barrier();
+    }
+    else if (type.equals("Time")) {
+      shootTime = 100;
+      return 10000;
+    }
+    else {
+      return 15000;
+    }
+    return 0;
   }
 }
